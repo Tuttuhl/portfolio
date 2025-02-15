0fbd900f06a625cbf61fe8d2e45b6f64aa294f37
Easton Tuttle <eastonmtuttle@gmail.com>
2025-02-15 17:08:05 -0600

    Added Prettier config file and formatted project

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,7 @@
 import * as motion from 'motion/react-client'
 
 export default function Navigation({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) {
@@ -10,7 +10,8 @@ export default function Navigation({
       initial={{ opacity: 0 }}
       animate={{ opacity: 100 }}
       transition={{ duration: 2.5 }}
-      className='bg-none flex flex-row w-full p-5 justify-evenly items-center lowercase'>
+      className="bg-none flex flex-row w-full p-5 justify-evenly items-center lowercase"
+    >
       {children}
     </motion.nav>
   )
