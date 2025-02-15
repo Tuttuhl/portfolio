800cdcdab41ee6be78d21042cafbdcfdbc220b40
Easton Tuttle <eastonmtuttle@gmail.com>
2025-02-15 17:05:15 -0600

    Add Motion to project and add fade animation to Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,11 +1,17 @@
+import * as motion from 'motion/react-client'
+
 export default function Navigation({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <nav className='bg-none flex flex-row w-full p-5 justify-evenly items-center'>
+    <motion.nav
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 100 }}
+      transition={{ duration: 2.5 }}
+      className='bg-none flex flex-row w-full p-5 justify-evenly items-center lowercase'>
       {children}
-    </nav>
+    </motion.nav>
   )
 }
