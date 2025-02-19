b955c6a268e1c3912a9fa51514ca5986c494b66e
Easton Tuttle <eastonmtuttle@gmail.com>
2025-02-19 17:57:16 -0600

    Populated Career and Projects sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,44 +7,139 @@ export default function Home() {
       <main className="px-10 flex flex-col gap-10">
         <section id="about">
           <h1 className="text-xl font-bold">about me</h1>
-          <p className="text-sm">software developer</p>
+          <p className="text-sm">
+            graduated with B.S. in Computer Science from the University of
+            Wyoming. software developer with 7 years working experience
+          </p>
+          <ol className="list-disc text-sm ps-10">
+            <li>building and web forms in HTML, CSS, and vanilla JS</li>
+            <li>
+              building and maintaining websites in React utilizing the NextJS
+              framework with Typescript
+            </li>
+            <li>
+              building and maintaining controller and minimal APIs with .NET 8.0
+            </li>
+            <li>
+              writing and maintaining SQL queries for API use and one-off use
+            </li>
+            <li>
+              utilization of Git Feature Branch Workflow for version control
+            </li>
+            <li>utilization of JIRA for project tracking</li>
+            <li>
+              utilization of both GitHub and BitBucket for remote source control
+            </li>
+          </ol>
         </section>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <section id="career">
+          <h1 className="text-xl font-bold">career</h1>
+          <div className="pb-5">
+            <h2 className="text-lg font-bold">System Admin Technician</h2>
+            <h3>Adams Bank & Trust</h3>
+            <h4 className="italic">May 2020 - Present</h4>
+            <p className="text-sm">
+              Developed custom internal web forms and their document routing
+              solutions using HTML, CSS, JavaScript, and Director Workflow.
+              Implemented new API endpoints for getting and posting data to and
+              from web forms in C# and .NET Core. Published updates and new
+              pages to the customer-facing website using HTML, CSS, and
+              JavaScript. Administered user security within the primary
+              documentation application used by bank employees.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">Customer Solutions Engineer</h2>
+            <h3>Handel Information Technologies</h3>
+            <h4 className="italic">January 2020 - May 2020</h4>
+            <p className="text-sm">
+              Implemented data conversions and system integrations with raw SQL
+              queries.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">Information Technology Intern</h2>
+            <h3>Adams Bank & Trust</h3>
+            <h4 className="italic">May 2019 - August 2019</h4>
+            <p className="text-sm">
+              Developed internal intranet web applications for displaying
+              equipment and client data to employees using C# and .NET Core.
+              Assisted in administration of user security rights within document
+              storage application.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">Development Intern</h2>
+            <h3>RCS</h3>
+            <h4 className="italic">June 2017 - August 2017</h4>
+            <p className="text-sm">
+              Implemented solutions for bugs and features within the Zetta
+              product utilizing C# and .NET Core. Utilized the scrum framework
+              from a developer perspective in order to prioritize tickets and
+              explain previous work and roadblocks to colleagues.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">Quality Assurance Tester</h2>
+            <h3>RCS</h3>
+            <h4 className="italic">May 2016 - August 2016</h4>
+            <p className="text-sm">
+              Completed manual bug, feature, and regression tests on flagship
+              products Zetta and GSelector. Utilized the scrum framework from a
+              quality assurance perspective in order to prioritize testing
+              tasks. Wrote and edited clear and concise testing instructions for
+              bug, feature, and regression tests.
+            </p>
+          </div>
+        </section>
+        <section id="projects">
+          <h1 className="text-xl font-bold">projects</h1>
+          <div className="pb-5">
+            <h2 className="text-lg font-bold">Adams Bank & Trust Website</h2>
+            <h3>
+              <Link href="https://abtbank.com/" className="hover:underline">
+                https://abtbank.com/
+              </Link>
+            </h3>
+            <p className="text-sm">
+              website built using NextJS, TailwindCSS, and Typescript. utilizes
+              Prismic headless content management system.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">MD Construction Website</h2>
+            <h3>
+              <Link
+                href="https://mdconstruction.biz/"
+                className="hover:underline"
+              >
+                https://mdconstruction.biz/
+              </Link>
+            </h3>
+            <p className="text-sm">
+              website built using NextJS, TailwindCSS, and Typescript. utilizes
+              Prismic headless content management system. hosted on Netlify.
+            </p>
+          </div>
+          <div className="py-5">
+            <h2 className="text-lg font-bold">Willow & Oak Events Website</h2>
+            <h3>
+              <Link
+                href="https://willowoakevents.com/"
+                className="hover:underline"
+              >
+                https://willowoakevents.com/
+              </Link>
+            </h3>
+            <p className="text-sm">
+              website built using NextJS, TailwindCSS, Typescript, and Motion
+              for animation. utilizes Prismic headless content management
+              system. hosted on Netlify.
+            </p>
+          </div>
+        </section>
+        <section id="scripts"></section>
+        <section id="contact"></section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
